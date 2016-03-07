8e4379a5e9d69cf5b6bade3d71a4d4d055325758
zrw <zrw2010@gmail.com>
2016-03-07 23:40:28 +0800

    [add] load text template file in dir and render

---
+++ text/template.go
@@ -0,0 +1,53 @@
+package render
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+var (
+	ErrTmplNotExist = errors.New("template not exist")
+)
+
+type TemplateRender struct {
+	tmpls map[string]*template.Template
+}
+
+// load template file *.tmpl in dir tmplDir and render
+func NewTemplateRender(tmplDir string) *TemplateRender {
+	fileInfo, err := ioutil.ReadDir(tmplDir)
+	if err != nil {
+		panic(err)
+	}
+	files := make([]string, 0, len(fileInfo))
+	for _, fi := range fileInfo {
+		name := fi.Name()
+		if strings.HasSuffix(name, ".tmpl") {
+			files = append(files, filepath.Join(tmplDir, name))
+		}
+	}
+	allTmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+	tmplRender := &TemplateRender{
+		tmpls: make(map[string]*template.Template),
+	}
+	tmpls := allTmpl.Templates()
+	for _, tmpl := range tmpls {
+		tmplRender.tmpls[tmpl.Name()] = tmpl
+	}
+	return tmplRender
+}
+
+func (r *TemplateRender) Render(w io.Writer, tmplName string, data interface{}) error {
+	t, ok := r.tmpls[tmplName]
+	if !ok {
+		return ErrTmplNotExist
+	}
+	return t.Execute(w, data)
+}
---
+++ text/template_test.go
@@ -0,0 +1,46 @@
+package render
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func genTemplateDir() (string, error) {
+	dir, err := ioutil.TempDir("", "tmpl_test")
+	if err != nil {
+		return "", err
+	}
+
+	tmpl1 := `{{ define "Test1" }}hello,{{ .Name }}{{ end }}`
+	tmpl2 := `{{ define "Test2" }}<body>{{ template "Test1" .Data }}</body>{{ end }}`
+	if err = ioutil.WriteFile(filepath.Join(dir, "1.tmpl"), []byte(tmpl1), 0666); err != nil {
+		return "", err
+	}
+	if err = ioutil.WriteFile(filepath.Join(dir, "2.tmpl"), []byte(tmpl2), 0666); err != nil {
+		return "", err
+	}
+	return dir, nil
+}
+
+func TestTemplateRender(t *testing.T) {
+	dir, err := genTemplateDir()
+	if err != nil {
+		t.FailNow()
+	}
+	defer os.RemoveAll(dir)
+	render := NewTemplateRender(dir)
+	err = render.Render(os.Stdout, "Test1", map[string]string{
+		"Name": "there",
+	})
+	if err != nil {
+		t.FailNow()
+	}
+	err = render.Render(os.Stdout, "Test2", map[string]interface{}{
+		"Data": map[string]string{"Name": "there"},
+	})
+	if err != nil {
+		t.FailNow()
+	}
+}
