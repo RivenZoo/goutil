410bd4dbff09d6e1e46271628a08391c41fda790
zrw <zrw2010@gmail.com>
2016-04-12 15:26:48 +0800

    [add] json response func

--- httputil/result.go
+++ httputil/result.go
@@ -33,3 +33,17 @@ func RedirectTo(uri string, w http.ResponseWriter) {
 	w.Header().Add("Location", uri)
 	w.WriteHeader(http.StatusFound)
 }
+
+func WriteContentType(w http.ResponseWriter, value []string) {
+	header := w.Header()
+	if val := header["Content-Type"]; len(val) == 0 {
+		header["Content-Type"] = value
+	}
+}
+
+var jsonContentType = []string{"application/json; charset=utf-8"}
+
+func WriteJSON(w http.ResponseWriter, obj interface{}) error {
+	WriteContentType(w, jsonContentType)
+	return json.NewEncoder(w).Encode(obj)
+}
