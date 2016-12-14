71e7b6e44d615d388db31cc40557b14cecdfef3e
zrw <zrw2010@gmail.com>
2016-12-14 16:57:51 +0800

    add example
    Signed-off-by: zrw <zrw2010@gmail.com>

---
+++ timestub/sectionstat/example_sectionstat_test.go
@@ -0,0 +1,29 @@
+package sectionstat_test
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/RivenZoo/goutil/timestub/sectionstat"
+)
+
+func Example_basic() {
+	// create section stat by axis
+	axis := []int{100, 200, 300, 500, 1000, 2000, 3000, 5000, 10000}
+	series := sectionstat.NewSectionSeries(axis)
+	stat := sectionstat.NewSectionStat(series)
+
+	times := 1000
+	for i := 0; i < times; i++ {
+		n := int64(rand.Int31n(int32(100000)))
+		// Collect is concurrency safe
+		stat.Collect(n)
+	}
+
+	fmt.Println("sum:%d,count:%d,mean:%d", stat.Sum(), stat.Count(), stat.Mean())
+	fmt.Println("section mean:%v", stat.SectionMean())
+
+	ratio := float32(0.99)
+	mean := stat.PercentMean(ratio)
+	fmt.Println("percent 0.99 mean:%d", mean)
+}
